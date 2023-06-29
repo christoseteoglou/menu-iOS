66aa24e8d43f5d76fd0fdbac5e643bbe5bb39060
christoseteoglou <itsmeglue@gmail.com>
2023-06-29 09:04:40 +0200

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Christos Eteoglou on 2023-06-29.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
