e28348a518ee2cbddb4a95f602bc74107a9348f2
christoseteoglou <itsmeglue@gmail.com>
2023-07-01 19:03:54 +0200

    Added the Menu List Row swift file

---
+++ Menu/MenuListRow.swift
@@ -0,0 +1,20 @@
+//
+//  MenuListRow.swift
+//  Menu
+//
+//  Created by Christos Eteoglou on 2023-06-30.
+//
+
+import SwiftUI
+
+struct MenuListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuListRow()
+    }
+}
